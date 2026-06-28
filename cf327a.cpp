37702ed2efa5d1ed88c7513df81b0c732a5f1242
Priyanshu Thakur <priyanshu.thakur@dataforall.org>
2026-06-28 21:52:34 +0530

    feat: add solutions for CF327A and CF433B, and implement binary search for CF474B

---
+++ cf327a.cpp
@@ -0,0 +1,29 @@
+/***********************************************************************************************************************************************/
+#include <bits/stdc++.h>
+#include <unordered_map>
+#include <unordered_set>
+#define ll long long int
+#define dl long double
+#define endl '\n'
+#define um unordered_map
+#define us unordered_set
+#define f0(j) for (ll i = 0; i < (ll)j; i++)
+#define f1(j) for (ll k = 0; k < (ll)j; k++)
+#define fk(k, j) for (ll i = k; i < (ll)j; i++)
+#define fa(a) for (auto x : a)
+using namespace std;
+const int mod = 1e9 + 7;
+/***********************************************************************************************************************************************/
+// 1 0 0 1 0 1
+void solve() {}
+
+int32_t main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  int t = 1;
+  // cin >> t;
+  while (t--) {
+    solve();
+  }
+  return 0;
+}
--- cf433b.cpp
+++ cf433b.cpp
@@ -1,5 +1,4 @@
 /***********************************************************************************************************************************************/
-#include <algorithm>
 #include <bits/stdc++.h>
 #include <unordered_map>
 #include <unordered_set>
@@ -15,47 +14,33 @@
 using namespace std;
 const int mod = 1e9 + 7;
 /***********************************************************************************************************************************************/
-// Took lil help (i think that was more than enough) basically what we have to
-// think is the continous number like 1 2 3 4 if been subtracted with their
-// positions they becam eall equal thats 0 0 0 0 or -1 -1 -1 -1 based on
-// indexing but yeah that question was literally wow
 
+void makepresum(ll n, vector<ll> &a) {
+  fk(2, n) { a[i] = a[i - 1] + a[i]; }
+  // fa(a) { cout << x << "   "; }
+  // cout << endl;
+}
 void solve() {
-  ll n, a, m, v3 = 1, v4 = 0;
-  vector<ll> v, v2;
+  ll n, a, m, type, l, r;
   cin >> n;
+  vector<ll> v, rv, v1, res;
   v.push_back(0);
+  rv.push_back(0);
   f0(n) {
     cin >> a;
-    v.push_back(a + v[i]);
+    v.push_back(a);
+    rv.push_back(a);
   }
+  sort(rv.begin(), rv.end());
+  makepresum(n + 1, v);
+  makepresum(n + 1, rv);
   cin >> m;
   f0(m) {
-    cin >> a;
-    v2.push_back(a);
+    cin >> type >> l >> r;
+    ll ans = type == 1 ? v[r] - v[l - 1] : rv[r] - rv[l - 1];
+    res.push_back(ans);
   }
-  //   while (v3 < n && v4 < m) {
-  //     if (v2[v4] <= v[v3] && v2[v4] > v[v3 - 1]) {
-  //       umap[v2[v4]] = v3;
-  //       v3++;
-  //       v4++;
-  //     } else {
-  //       v3++;
-  //     }
-  //   }
-  f0(m) {
-    a = v2[i];
-    ll l = 0, h = n, mid;
-    while (l < h) {
-      mid = l + (h - l) / 2;
-      if (a <= v[mid]) {
-        h = mid;
-      } else {
-        l = mid + 1;
-      }
-    }
-    cout << l << endl;
-  }
+  fa(res) { cout << x << endl; }
 }
 
 int32_t main() {
