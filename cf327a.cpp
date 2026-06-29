28f3e155bed399bb16c8e5dbf0e4eda150713c14
Priyanshu Thakur <priyanshu.thakur@dataforall.org>
2026-06-29 11:35:57 +0530

    feat: implement solution for Codeforces 327A using dynamic programming

--- cf327a.cpp
+++ cf327a.cpp
@@ -1,5 +1,6 @@
 /***********************************************************************************************************************************************/
 #include <bits/stdc++.h>
+#include <climits>
 #include <unordered_map>
 #include <unordered_set>
 #define ll long long int
@@ -14,8 +15,34 @@
 using namespace std;
 const int mod = 1e9 + 7;
 /***********************************************************************************************************************************************/
-// 1 0 0 1 0 1
-void solve() {}
+void solve() {
+  ll n, a, countone = 0, maxm;
+  cin >> n;
+  vector<ll> v, dp(n, 0);
+  f0(n) {
+    cin >> a;
+    v.push_back(a);
+    if (a == 1)
+      countone++;
+  }
+  if (countone == n) {
+    countone--;
+    cout << countone << endl;
+    return;
+  }
+  dp[0] = v[0] == 0 ? 1 : 0;
+  maxm = v[0] == 0 ? 1 : 0;
+  fk(1, n) {
+    if (v[i] == 1)
+      dp[i] = dp[i - 1] - 1;
+    else
+      dp[i] = max((ll)1, dp[i - 1] + 1);
+    maxm = max(maxm, dp[i]);
+  }
+  countone += maxm;
+
+  cout << countone << endl;
+}
 
 int32_t main() {
   ios_base::sync_with_stdio(false);
